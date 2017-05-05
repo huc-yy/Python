865769310880c564283d964618cf8ce4d4153ff9
huc-yy <411657800@qq.com>
2017-05-05 09:58:58 +0800

    Create inherit.py

---
+++ inherit.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+
+class SchoolMember:
+	'''Represent any school member.'''
+	def __init__(self,name,age):
+		self.name = name
+		self.age = age
+		print '(Initialized SchoolMember: %s)' % self.name
+		
+	def tell(self):
+		'''Tell my details.'''
+		print 'Name:"%s" Age:"%s"' % (self.name, self.age),
+		
+class Teacher(SchoolMember):
+	'''Represents a teacher.'''
+	def __init__(self,name,age,salary):
+		SchoolMember.__init__(self,name,age)
+		self.salary = salary
+		print '(Initialized Teacher: %s)' % self.name
+		
+		def tell(self):
+		SchoolMember.tell(self)
+		print 'Salary: "%d"' % self.salary
+		
+class Student(SchoolMember):
+	'''Represent a student.'''
+	def __init__(self,name,age,marks):
+		SchoolMember.__init__(self,name,age)
+		self.marks = marks
+		print '(Initialized Student: %s)' % self.name
+		
+	def tell(self):
+		SchoolMember.tell(self)
+		print 'Marks: "%d"' % self.marks
+		
+t = Teacher('Baby', 40, 30000)
+s = Student('Nice', 22, 75)
+
+print # prints a blank line
+
+members = [t, s]
+for member in members:
+	member.tell() # work for both Teachers and Students
